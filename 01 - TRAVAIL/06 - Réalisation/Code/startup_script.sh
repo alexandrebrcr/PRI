cca771ee9ef1dcce2c591a99070506a34ffbe3be
alexandrebrcr <alexandre.bourcier989@gmail.com>
2026-01-18 22:31:13 +0100

    startup

--- 01 - TRAVAIL/06 - Réalisation/Code/startup_script.sh
+++ 01 - TRAVAIL/06 - Réalisation/Code/startup_script.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Annonce immédiate du démarrage (le plus tôt possible)
+# Carte son 2 (USB)
+if [ -x "$(command -v pico2wave)" ]; then
+    pico2wave -w /dev/shm/sys_boot.wav -l fr-FR "Démarrage du système"
+    aplay -D plughw:2,0 /dev/shm/sys_boot.wav 2>/dev/null
+    rm -f /dev/shm/sys_boot.wav
+fi
+
 # Définition du dossier du projet
 PROJECT_DIR="/home/canneblancheintelligente/Documents/PRI_ALEXANDRE/PRI/01 - TRAVAIL/06 - Réalisation/Code"
 
-sleep 10
+sleep 3
 
 # Donne les permissions nécessaires au port série (Ultrasons)
 if [ -e /dev/ttyTHS1 ]; then
